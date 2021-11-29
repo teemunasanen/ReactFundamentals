1e8f4cf4585c55e925ad112db6d3f4f6c57f31d4
Teemu Nasanen <teemu.nasanen@student.laurea.fi>
2021-11-29 13:55:06 +0200

    TASK: Button Component - install, import, use

--- 7_3rd_Party_Components/todogrid/src/App.js
+++ 7_3rd_Party_Components/todogrid/src/App.js
@@ -5,6 +5,9 @@ import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
+import { AwesomeButton } from 'react-awesome-button';
+import 'react-awesome-button/dist/styles.css';
+
 function App() {
   const [todo, setTodo] = useState({description: '', date: '', status: ''});
   const [todos, setTodos] = useState([]);
@@ -23,7 +26,7 @@ function App() {
       <input placeholder="Description" name="description" value={todo.description} onChange={inputChanged} />
       <input placeholder="Date" name="date" value={todo.date} onChange={inputChanged}/>
       <input placeholder="Status" name="status" value={todo.status} onChange={inputChanged}/>
-      <button onClick={addTodo}>Add</button>
+      <AwesomeButton type='facebook' onClick={addTodo}>Add</AwesomeButton>
       <div className='ag-theme-material' style={{ height: 600, width: 600, margin: 'auto' }}>
         <AgGridReact rowData={todos} animateRows={true}>
           <AgGridColumn field='description' suppressMovable={true} sortable={true} filter={true}></AgGridColumn>
