2bb92763708ff0194f8c33fc79365e567e028393
Teemu Nasanen <teemu.nasanen@student.laurea.fi>
2021-11-29 15:49:18 +0200

    ADD component NotFound.js EDIT app with Router v6

--- 8_React_Router/reactrouting/src/App.js
+++ 8_React_Router/reactrouting/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import NotFound from './components/NotFound';
+import { BrowserRouter, Route, Link, Routes } from 'react-router-dom'; // React Router v6
+
 
 function App() {
   return (
     <div className="App">
-     
+      <BrowserRouter>
+        <div>
+          <Link to='/'>Home</Link>{' '}
+          <Link to='/contact'>Contact</Link>{' '}
+          <Link to='/about'>About</Link>{' '}
+          <Routes>
+            <Route exact path='/' element={<Home/>} />
+            <Route path='/contact' element={<Contact/>} />
+            <Route path='/about' element={<About/>} />
+            <Route path='*' element={<NotFound/>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
