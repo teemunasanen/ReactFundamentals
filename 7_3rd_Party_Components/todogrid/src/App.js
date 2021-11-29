d543682eb7f14481dc4547a590fcf67a75d14117
Teemu Nasanen <teemu.nasanen@student.laurea.fi>
2021-11-29 13:24:03 +0200

    TASK 1 prevent dragging columns

--- 7_3rd_Party_Components/todogrid/src/App.js
+++ 7_3rd_Party_Components/todogrid/src/App.js
@@ -26,9 +26,9 @@ function App() {
       <button onClick={addTodo}>Add</button>
       <div className='ag-theme-material' style={{ height: 600, width: 600, margin: 'auto' }}>
         <AgGridReact rowData={todos}>
-          <AgGridColumn field='description' sortable={true} filter={true}></AgGridColumn>
-          <AgGridColumn field='date' sortable={true} filter={true}></AgGridColumn>
-          <AgGridColumn field='status' sortable={true} filter={true}></AgGridColumn>
+          <AgGridColumn field='description' suppressMovable={true} sortable={true} filter={true}></AgGridColumn>
+          <AgGridColumn field='date' suppressMovable={true} sortable={true} filter={true}></AgGridColumn>
+          <AgGridColumn field='status' suppressMovable={true} sortable={true} filter={true}></AgGridColumn>
         </AgGridReact>
       </div>
     </div>
