73b2aae754f2bdc058a7aadfcde98722f0a25279
Teemu Nasanen <teemu.nasanen@student.laurea.fi>
2021-12-01 13:58:33 +0200

    FIREBASETODO guided build AddTodo.js -handle open/close -useState (addtodo) -inputChanged -button, textfield, dialogs

---
+++ 10_React-Firebase/firebasetodo/src/AddTodo.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+
+function AddTodo() {
+  const [open, setOpen] = useState(false);
+  const [todo, setTodo] = useState({ description: "", date: "", priority: "" });
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const inputChanged = (event) => {
+    setTodo({ ...todo, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={handleOpen}>
+        Add todo
+      </Button>
+      <Dialog open={open}>
+        <DialogTitle>New todo</DialogTitle>
+        <DialogContent>
+          <TextField
+            name="description"
+            value={todo.description}
+            onChange={inputChanged}
+            margin="dense"
+            label="Description"
+            fullWidth
+          />
+          <TextField
+            name="date"
+            value={todo.date}
+            onChange={inputChanged}
+            margin="dense"
+            label="Date"
+            fullWidth
+          />
+          <TextField
+            name="priority"
+            value={todo.priority}
+            onChange={inputChanged}
+            margin="dense"
+            label="Priority"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button color="primary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button color="primary" onClick={handleClose}>
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+export default AddTodo;
--- 10_React-Firebase/firebasetodo/src/App.js
+++ 10_React-Firebase/firebasetodo/src/App.js
@@ -3,6 +3,7 @@ import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 import Appbar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import AddTodo from './AddTodo';
 
 import './App.css';
 import 'ag-grid-community/dist/styles/ag-grid.css';
@@ -29,6 +30,7 @@ function App() {
           <Typography variant='h5' noWrap>TodoList</Typography>
         </Toolbar>
       </Appbar>
+      <AddTodo />
       <div className='ag-theme-material' style={{ height: 400, width: 600, margin: 'auto' }}>
         <AgGridReact rowData={todos}>
           <AgGridColumn sortable={true} filter={true} field='description' />
