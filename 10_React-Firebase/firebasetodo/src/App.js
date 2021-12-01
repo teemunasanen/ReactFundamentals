41c6e4343f3b2f965db4e7b9eac809962024efea
Teemu Nasanen <teemu.nasanen@student.laurea.fi>
2021-12-01 13:16:05 +0200

    FIREBASETODO guided build fetchItems, useState, useEffect, AgGrid

--- 10_React-Firebase/firebasetodo/src/App.js
+++ 10_React-Firebase/firebasetodo/src/App.js
@@ -1,9 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+
 import './App.css';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css'
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetchItems();
+  }, [])
+
+  const fetchItems = () => {
+    fetch('https://todolist-65719-default-rtdb.europe-west1.firebasedatabase.app/items/.json')
+    .then(response => response.json())
+    .then(data => setTodos(Object.values(data)))
+    .catch(err => console.error(err))
+  }
+
   return (
     <div className="App">
-      
+      <div className='ag-theme-material' style={{ height: 400, width: 600, margin: 'auto' }}>
+        <AgGridReact rowData={todos}>
+          <AgGridColumn sortable={true} filter={true} field='description' />
+          <AgGridColumn sortable={true} filter={true} field='date' />
+          <AgGridColumn sortable={true} filter={true} field='priority' />
+        </AgGridReact>
+      </div>
     </div>
   );
 }
