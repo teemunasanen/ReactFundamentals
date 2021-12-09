c31e48359f1f6e33e2907a3c38a3221bb5c6d1ec
Teemu Nasanen <teemu.nasanen@student.laurea.fi>
2021-12-09 12:35:05 +0200

    REACT COMPONENT initial commit - Render h1 and p elements - modify css file: h1 color, p font-size - README

---
+++ 3_React_Component/component/README.md
@@ -0,0 +1,6 @@
+# React Component
+
+>### Exercises
+>* Create React app
+>* Render h1 and p elements
+>* Change h1 text color to blue and p font-size to 20px in CSS-file
---
+++ 3_React_Component/component/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my site</h1>
+      <p>This is my first React app</p>
+    </div>
+  );
+}
+
+export default App;
